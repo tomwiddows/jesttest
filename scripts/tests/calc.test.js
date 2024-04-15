017127529d8cb5825c57c5d143b1418a2874bc72
Tom Widdows <thomas.widdows10@gmail.com>
2024-04-15 14:08:09 +0000

    Add simple HTML page to test. Add button function to buttin.js. Add button test file.

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -4,6 +4,9 @@ describe("Calculator", () => {
     describe("Addition", () => {
         test("Should return 42 for 20 + 22", () => {
             expect(addition(20,22)).toBe(42);
-        })
+        });
+        test("Should return 73 for 42 + 31", () => {
+            expect(addition(42,31)).toBe(73);
+        });
     })
 })
